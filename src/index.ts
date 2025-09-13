8eb2958baa001310af2f8e93b0a641905ae35441
GunawanMayo <Qwanser3301@gmail.com>
2025-09-13 11:23:34 +0900

    add env file

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,42 @@
 import express , { Express } from "express";
-import { AddressInfo } from "net";
 import cors from "cors";
 import mysql, { Connection } from "mysql2/promise";
+import * as dotenv from "dotenv";
 
 // const express = require("express");
 // const cors = require("cors");
 // const mysql2 = require("mysql2");
 
 async function main() {
+  dotenv.config();
+  const{PORT, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB} = process.env;
   const app: Express =  express();
 
-  app.listen(3000,function() {
-    console.log("Node.js is listening to PORT: " + "3000")
+  app.listen(parseInt(PORT as string),function() {
+    console.log(("Node.js is listening to PORT: " + PORT) as string);
   });
 
   app.disable("x-powered-by")
   app.use(cors()).use(express.json());
 
   const connection : Connection =  await mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "user",
-  password: "password",
-  database: "sample"
+  host: MYSQL_HOST,
+  port: parseInt(MYSQL_PORT as string),
+  user: MYSQL_USER,
+  password: MYSQL_PASS,
+  database: MYSQL_DB,
   });
 
-  const sql = "select * FROM todos";
-  const result = await connection.execute(sql);
+  // const sql = "select * FROM todos";
+  // const result = await connection.execute(sql);
 
-  console.log(result);
+  // console.log(result);
+
+  app.get("/api/todos", async(req,res) => {
+    const sql = "select * FROM todos";
+    const [rows]= await connection.execute(sql);
+    res.json(rows);
+  });
 
 //   app.get('/', (req, res) => {
 //   const sql = `
